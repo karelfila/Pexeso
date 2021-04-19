f9b513aa0c8e96139910f1bdefd4d94a7ceb73bd
karelfila <karel.fila@educanet.cz>
2021-04-19 17:21:40 +0200

    Dokončeno

--- Main.js
+++ Main.js
@@ -1,47 +1,56 @@
 function didLoad() {
 
     let emojis = ["😂", "🥰", "🤔", "🙂", "😍", "😎", "🤩", "😃"];
+    let emojis2 = [];
     let r;
-    let a = 7;
     let lng = emojis.length;
+    let pocet = 0;
 
     for (let i = 0; i < lng; i++) {
         emojis.push(emojis[i]);
-
-    }
-    console.log(emojis)
-
-    for (let i = 0; i < emojis.length; i++) {
-        r = Math.floor((Math.random() * lng));
-        lng -= 1;    
-
     }
 
+    while (emojis.length !== 0) {
+        r = Math.floor((Math.random() * emojis.length));
+        emojis2.push(emojis[r]);
+
+        pocet++;
+        emojis.splice(r, 1);
+    }
+    console.log(emojis2);
+    console.log(emojis);
 
 
-    /* for (let i = 0; i < emojis.length; i++) {
-         let mujdiv = document.createElement("div");
-         mujdiv.className = "mujdiv";
-         r = Math.floor((Math.random() * a));
-         mujdiv.innerText = emojis[i];
-         document.body.appendChild(mujdiv);
-         emojis.splice(r, 1);
-         a = a - 1;
-     }
-     for (let i = 0; i < emojis.length; i++) {
-         let mujdiv1 = document.createElement("div");
-         mujdiv1.className = "mujdiv1";
+    /*
+         for (let i = 0; i < emojis.length; i++) {
+             let mujdiv = document.createElement("div");
+             mujdiv.className = "mujdiv";
+             r = Math.floor((Math.random() * a));
+             mujdiv.innerText = emojis[i];
+             document.body.appendChild(mujdiv);
+             emojis.splice(r, 1);
+             a = a - 1;
+         }
+         for (let i = 0; i < emojis.length; i++) {
+             let mujdiv1 = document.createElement("div");
+             mujdiv1.className = "mujdiv1";
 
-         mujdiv1.innerText = emojis[i];
-         document.body.appendChild(mujdiv1);
-     }
-     */
+             mujdiv1.innerText = emojis[i];
+             document.body.appendChild(mujdiv1);
+         }
+    */
     let all = document.createElement("div");
     for (let i = 0; i < lng * 2; i++) {
+
         let mujdiv = document.createElement("div");
 
-        mujdiv.className = "mujdiv";
-        mujdiv.innerText = emojis[i];
+        if((i + 1)%4 == 0) {
+            mujdiv.className = "mujdiv1";
+            mujdiv.innerHTML = emojis2[i];
+        } else {
+            mujdiv.className = "mujdiv";
+            mujdiv.innerText = emojis2[i];
+        }
         all.appendChild(mujdiv);
         all.setAttribute("id", "Id");
     }
